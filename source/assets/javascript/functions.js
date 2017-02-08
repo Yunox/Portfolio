bfdfecbaadb49ca931b1ebf45bcaa5a437b91ed5
Yunox <david.riege@web.de>
2017-02-08 17:16:20 +0100

    Added new elements
    Added Logo and new font colors. Working on navigation in progress

--- source/assets/javascript/functions.js
+++ source/assets/javascript/functions.js
@@ -7,4 +7,28 @@ $(function() {
       $('.main-nav').toggleClass('main-nav--active');
     });
 
+    $('img.svg').each(function(){
+      var $img = $(this),
+      imgID = $img.attr('id'),
+      imgClass = $img.attr('class'),
+      imgURL = $img.attr('src');
+
+      $.get(imgURL, function(data) {
+
+        var $svg = $(data).find('svg');
+
+        if(typeof imgID !== 'undefined') {
+          $svg = $svg.attr('id', imgID);
+        }
+        if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        $svg = $svg.removeAttr('xmlns:a');
+
+        $img.replaceWith($svg);
+});
+
+});
+
 });
